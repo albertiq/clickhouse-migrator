b38e44f59e6d7b7c8a4697d7337305af8fd71796
albertiq <alberttuygunov@gmail.com>
2024-09-02 11:32:20 +0500

    edited secure fields in config

--- src/clickhouse_migrate/config.py
+++ src/clickhouse_migrate/config.py
@@ -7,16 +7,32 @@ from typing import Optional
 @dataclass
 class ClickhouseClientSettings:
     host: str = field(default=os.getenv("DB_CLICKHOUSE_HOST", "localhost"))
-    port: int = field(default=os.getenv("DB_CLICKHOUSE_PORT", 9000))
+    port: int = field(default=os.getenv("DB_CLICKHOUSE_PORT"), init=False)
+    secure_port: int = field(default=os.getenv("DB_CLICKHOUSE_SECURE_PORT"))
     user: str = field(default=os.getenv("DB_CLICKHOUSE_USER", "default"))
     password: str = field(default=os.getenv("DB_CLICKHOUSE_PASS", ""))
     database: str = field(default=os.getenv("DB_CLICKHOUSE_NAME", "dwh"))
     migrations_home: str = field(default=os.getenv("DB_CLICKHOUSE_SQL_PATH", Path("./sql/")))
-    secure: Optional[bool] = field(default=os.getenv("DB_CLICKHOUSE_SECURE"))
-    verify: Optional[bool] = field(default=os.getenv("DB_CLICKHOUSE_VERIFY"))
-    ca_certs: Optional[str] = field(default=os.getenv("DB_CLICKHOUSE_CA_CERTS", Path("./certs/clickhouse-ca.crt")))
-    certfile: Optional[str] = field(default=os.getenv("DB_CLICKHOUSE_CERT_FILE", Path("./certs/clickhouse.crt")))
-    keyfile: Optional[str] = field(default=os.getenv("DB_CLICKHOUSE_KEY_FILE", Path("./certs/clickhouse.key")))
+    secure: Optional[bool] = field(init=False)
+    verify: Optional[bool] = field(init=False)
+    ca_certs: Optional[str] = field(default=os.getenv("DB_CLICKHOUSE_CA_CERTS"))
+    certfile: Optional[str] = field(default=os.getenv("DB_CLICKHOUSE_CERT_FILE"))
+    keyfile: Optional[str] = field(default=os.getenv("DB_CLICKHOUSE_KEY_FILE"))
+    is_secure: Optional[bool] = field(default=os.getenv("IS_SECURE_CLICKHOUSE"), init=False)
+
+    def __post_init__(self):
+        self.secure = self.get_bool_env("DB_CLICKHOUSE_SECURE", False)
+        self.verify = self.get_bool_env("DB_CLICKHOUSE_VERIFY", False)
+        self.is_secure = self.get_bool_env("IS_SECURE_CLICKHOUSE", False)
+
+        self.port = self.secure_port if self.is_secure else self.port
+
+    @staticmethod
+    def get_bool_env(key: str, default: bool) -> bool:
+        str_env = os.getenv(key)
+        if str_env:
+            return str_env.lower() in ("true", "1")
+        return default
 
 
 clickhouse_client_config = ClickhouseClientSettings()
